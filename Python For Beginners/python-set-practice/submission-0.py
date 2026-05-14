28ded2b1141dbf59b462f4fcc690988f8700c495
roshan-1001 <roshanhingnekar07@gmail.com>
2026-05-14 20:36:40 +0530

    Add: python-set-practice - submission-0

---
+++ Python For Beginners/python-set-practice/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def contains_duplicate(words: List[str]) -> bool:
+    my_set = set()
+    for word in words:
+        my_set.add(word)
+    if len(words)>len(my_set):
+        return True
+    
+    return False
+
+# do not modify code below this line
+print(contains_duplicate(["hello", "world", "hello"]))
+print(contains_duplicate(["hello", "world", "i", "am", "great"]))
+print(contains_duplicate(["hello", "hello", "hello"]))
+print(contains_duplicate(["Hello", "hellooo", "hello"]))
