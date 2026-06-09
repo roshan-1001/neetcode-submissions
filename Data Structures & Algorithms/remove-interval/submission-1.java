a48d751494ea9ffea0f2f21e05a2bb44cc54f95a
roshan-1001 <roshanhingnekar07@gmail.com>
2026-06-09 21:24:02 +0530

    Add: remove-interval - submission-1

---
+++ Data Structures & Algorithms/remove-interval/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+        int left = toBeRemoved[0];
+        int right = toBeRemoved[1];
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int[] interval: intervals){
+            if (right <= interval[0] ||left >= interval[1] ){
+                res.add(Arrays.asList(interval[0],interval[1]));
+            }
+            else{
+                if (interval[0] < left) {
+                    res.add(Arrays.asList(interval[0], left));
+                }
+                // right piece remains
+                if (interval[1] > right) {
+                    res.add(Arrays.asList(right, interval[1]));
+                }
+            }
+
+        }
+        return res;
+    }
+}
